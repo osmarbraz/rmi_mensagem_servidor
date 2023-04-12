96cf04e1ecaeb798edb5fa88611f38467d938f7a
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-04-12 08:26:31 -0300

    Início do projeto

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,4 +1,6 @@
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -25,8 +27,10 @@ public class Principal {
             //Registra o stub no rmiregistry
             registro.bind(nomeServico, stub); 
             System.out.println("Servidor no ar. Nome do objeto no servidor: \'" + nomeServico + "\'");
-        } catch (Exception ex) {
-            System.out.println("Excecao: " + ex.getMessage());
+        } catch (RemoteException re) {
+            System.out.println("Excecao: " + re.getMessage());
+        } catch (AlreadyBoundException abe) {
+            System.out.println("Excecao: " + abe.getMessage());
         }
     }
 }
